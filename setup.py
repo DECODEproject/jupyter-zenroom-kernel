c36b7fae8dd45924bfcda3b0357993ee9bd279f0
Puria Nafisi Azizi <nafisi@gmail.com>
2018-04-17 06:58:08 +0200

    Adding readme and some links for the release on pypi

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 from setuptools import setup, find_packages
 
-try:
-    import pypandoc
-    description = pypandoc.convert('README.md', 'rst')
-except (IOError, ImportError):
-    description = ''
-
 setup(
     name='jupyter_zenroom_kernel',
     version='0.0.1',
     description='Jupyter kernel for Zenroom. Small, secure and portable virtual machine for crypto language processing',
-    long_description=description,
     author='Dyne development team',
     author_email='puria@dyne.org',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    project_urls={
+        'Tracker': 'https://github.com/puria/jupyter-zenroom-kernel/issues',
+        'Zenroom project': 'https://zenroom.dyne.org/',
+        'Decode Project': 'https://decodeproject.eu/',
+    },
     url='https://github.com/puria/jupyter-zenroom-kernel',
     license='AGPLv3+',
     keywords='',
     platforms='Linux, Mac OS X, Windows',
+    python_requires='>=3',
     install_requires=[
         'metakernel==0.20.14',
         'notebook>=5.4.1',
@@ -30,7 +31,6 @@ setup(
         'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: GNU Affero General Public License v3',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Zenroom',
         'Topic :: Security :: Cryptography',
         'Topic :: System :: Distributed Computing',
     ]
